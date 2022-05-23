b2012a3ccb84e6b36a6672eeafe10d29dcc48c23
Cash DeLeon <cashd@users.noreply.github.com>
2022-05-23 12:54:27 -0500

    timelock events and constants

--- contracts/TimeLock.sol
+++ contracts/TimeLock.sol
@@ -13,4 +13,35 @@ contract TimeLock {
   error TimestampExpiredError(uint blockTimestamp, uint expiresAt);
   error TxFailedError();
 
+  /*
+    | Events | 
+  */
+  event Queue (
+    bytes32 indexed txId,
+    address indexed target,
+    uint value,
+    string func,
+    bytes data,
+    uint timestamp
+  );
+
+  event Execute(
+    bytes32 indexed txId,
+    bytes32 indexed target,
+    uint value,
+    string func,
+    bytes32 data,
+    uint timestamp
+  );
+
+   event Cancel(bytes32 indexed txId);
+
+  /*
+    | Constants | 
+  */
+    uint public constant MIN_DELAY = 10; // seconds
+    uint public constant MAX_DELAY = 1000; // seconds
+    uint public constant GRACE_PERIOD = 1000; // seconds
+
+
 }
