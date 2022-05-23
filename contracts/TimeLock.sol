6c853d5f18f2f5c296ddd203b5d27f5c07eac834
Cash DeLeon <cashd@users.noreply.github.com>
2022-05-23 13:59:17 -0500

    Update TimeLock.sol

--- contracts/TimeLock.sol
+++ contracts/TimeLock.sol
@@ -2,46 +2,92 @@
 pragma solidity ^0.8.6;
 
 contract TimeLock {
-  /*
+    /*
     | Errors | 
   */
-  error NotOwnerError();
-  error AlreadyQueuedError(bytes32 txId);
-  error TimestampNotInRangeError(uint blockTimestamp, uint timestamp);
-  error NotQueuedError(bytes32 txId);
-  error TimestampNotPassedError(uint blockTimestmap, uint timestamp);
-  error TimestampExpiredError(uint blockTimestamp, uint expiresAt);
-  error TxFailedError();
+    error NotOwnerError();
+    error AlreadyQueuedError(bytes32 txId);
+    error TimestampNotInRangeError(uint256 blockTimestamp, uint256 timestamp);
+    error NotQueuedError(bytes32 txId);
+    error TimestampNotPassedError(uint256 blockTimestmap, uint256 timestamp);
+    error TimestampExpiredError(uint256 blockTimestamp, uint256 expiresAt);
+    error TxFailedError();
 
-  /*
+    /*
     | Events | 
   */
-  event Queue (
-    bytes32 indexed txId,
-    address indexed target,
-    uint value,
-    string func,
-    bytes data,
-    uint timestamp
-  );
+    event Queue(
+        bytes32 indexed txId,
+        address indexed target,
+        uint256 value,
+        string func,
+        bytes data,
+        uint256 timestamp
+    );
 
-  event Execute(
-    bytes32 indexed txId,
-    bytes32 indexed target,
-    uint value,
-    string func,
-    bytes32 data,
-    uint timestamp
-  );
+    event Execute(
+        bytes32 indexed txId,
+        bytes32 indexed target,
+        uint256 value,
+        string func,
+        bytes32 data,
+        uint256 timestamp
+    );
 
-   event Cancel(bytes32 indexed txId);
+    event Cancel(bytes32 indexed txId);
 
-  /*
+    /*
     | Constants | 
   */
-    uint public constant MIN_DELAY = 10; // seconds
-    uint public constant MAX_DELAY = 1000; // seconds
-    uint public constant GRACE_PERIOD = 1000; // seconds
+    uint256 public constant MIN_DELAY = 10; // seconds
+    uint256 public constant MAX_DELAY = 1000; // seconds
+    uint256 public constant GRACE_PERIOD = 1000; // seconds
 
+    address public owner;
+    mapping(bytes32 => bool) public queued;
 
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != owner) {
+            revert NotOwnerError();
+        }
+        _;
+    }
+
+    receive() external payable {}
+
+    function getTxId(
+        address _target,
+        uint256 _value,
+        string calldata _func,
+        bytes calldata _data,
+        uint256 _timestamp
+    ) public pure returns (bytes32) {
+        return keccak256(abi.encode(_target, _value, _func, _data, _timestamp));
+    }
+
+    function queue(
+        address _target,
+        uint256 _value,
+        string calldata _func,
+        bytes calldata _data,
+        uint256 _timestamp
+    ) external onlyOwner returns (bytes32 txId) {
+        txId = getTxId(_target, _value, _func, _data, _timestamp);
+        if (queued[txId]) {
+            revert AlreadyQueuedError(txId);
+        }
+        if (
+            _timestamp < block.timestamp + MIN_DELAY ||
+            _timestamp > block.timestamp + MAX_DELAY
+        ) {
+            revert TimestampNotInRangeError(block.timestamp, _timestamp);
+            queued[txId] = true;
+
+            emit Queue(txId, _target, _value, _func, _data, _timestamp);
+        }
+    }
 }
