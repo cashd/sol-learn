b354a7bf559efb48c47e80d25cec5da1eaa7fa58
Cash DeLeon <cashd@users.noreply.github.com>
2022-06-02 17:56:39 -0500

    prettier

--- test/proxy.test.ts
+++ test/proxy.test.ts
@@ -1,10 +1,10 @@
-import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
-import { expect } from 'chai';
-import { Contract } from 'ethers';
-import { ethers } from 'hardhat';
-import { Logic, Proxy } from '../typechain';
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+import { Logic, Proxy } from "../typechain";
 
-describe('Proxy contract', async () => {
+describe("Proxy contract", async () => {
     let owner: SignerWithAddress;
     let proxy: Proxy;
     let logic: Logic;
@@ -12,32 +12,32 @@ describe('Proxy contract', async () => {
     beforeEach(async () => {
         [owner] = await ethers.getSigners();
 
-        const logicFactory = await ethers.getContractFactory('Logic');
+        const logicFactory = await ethers.getContractFactory("Logic");
         logic = await logicFactory.deploy();
         await logic.deployed();
 
-        const proxyFactory = await ethers.getContractFactory('Proxy');
+        const proxyFactory = await ethers.getContractFactory("Proxy");
         proxy = await proxyFactory.deploy();
         await proxy.deployed();
 
         await proxy.setImpl(logic.address);
 
-        const abi = ['function initialize() public'];
+        const abi = ["function initialize() public"];
         const proxied = new Contract(proxy.address, abi, owner);
         await proxied.initialize();
     });
 
-    it('points to an implementation contract', async () => {
+    it("points to an implementation contract", async () => {
         expect(await proxy.getImpl()).to.eq(logic.address);
     });
 
-    it('number in logic contract is correct', async () => {
+    it("number in logic contract is correct", async () => {
         const abi = [
-            'function setNumber(uint256 _number) public',
-            'function getNumber() public view returns (uint256)',
+            "function setNumber(uint256 _number) public",
+            "function getNumber() public view returns (uint256)",
         ];
         const proxied = new ethers.Contract(proxy.address, abi, owner);
 
-        expect(await proxied.getNumber()).to.eq('0x42');
+        expect(await proxied.getNumber()).to.eq("0x42");
     });
 });
